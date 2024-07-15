691f9ced13623aded3d64197732ed58c6f9acd71
Adeyemi Adekunle <adeyemisuraju15@gmail.com>
2024-07-15 08:33:32 +0100

    fix: nav button

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,14 @@
 'use client'
-import { Cell } from "@telegram-apps/telegram-ui";
 import { Link } from '@/components/Link/Link';
 function Navigation() {
     return (
-        <div className="py-3 flex justify-between border-t">
-            <button>  <Link href='/'>Earn</Link></button>
-            <button> <Link href='/invite'>Invite</Link></button>
-            <button> <Link href='/play'>Play</Link></button>
-            <button> <Link href='/rewards'>Rewards</Link></button>
+        <div className="navigation h-14 mb-4 flex flex-col border fixed bottom-0 left-0 right-0 w-full">
+            <div className='flex justify-between mx-3 p-3 h-full rounded-md'>
+                <button><Link href='/'>Earn</Link></button>
+                <button><Link href='/invite'>Invite</Link></button>
+                <button><Link href='/play'>Play</Link></button>
+                <button><Link href='/rewards'>Rewards</Link></button>
+            </div>
         </div>
     );
 }
